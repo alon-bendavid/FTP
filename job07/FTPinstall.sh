c2dd6d36e2f5b51a33767f86063aa98600c91b20
alon-bendavid <ben-david.alon@laplateforme.io>
2022-10-07 06:42:57 -0500

    script update

--- job07/FTPinstall.sh
+++ job07/FTPinstall.sh
@@ -3,18 +3,20 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install wget -y
 sudo apt-get update && apt-get install proftpd
+
 rm /etc/proftpd/proftpd.conf
-cd /etc/proftpd
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=12mEH_CfWr4Dnl--bj6MsbzbHdHm1E0OH' -O proftpd.conf
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=12mEH_CfWr4Dnl--bj6MsbzbHdHm1E0OH' -O /etc/proftpd/proftpd.conf
 sudo service proftpd restart
 
 #installing TLS
+
 mkdir /etc/proftpd/ssl
 openssl req -new -x509 -days 365 -nodes -out /etc/proftpd/ssl/proftpd.cert.pem -keyout /etc/proftpd/ssl/proftpd.key.pem
 chmod 600 /etc/proftpd/ssl/proftpd.*
 rm /etc/proftpd/tls.conf
-cd /etc/proftpd
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QlEB3du_D-Ab8aPzshm73T_QMaLKWW3p' -O tls.conf
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QlEB3du_D-Ab8aPzshm73T_QMaLKWW3p' -O /etc/proftpd/tls.conf
 sudo service proftpd restart
 
 echo "FTPs installation complete"
